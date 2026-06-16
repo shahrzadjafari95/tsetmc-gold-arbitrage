d12f9a147e45613d8cf743201f76ffdca97585ad
shahrzadjafari <jafari.shahrzad74@gmail.com>
2026-06-16 15:54:14 +0330

    Add functionality to fetch live XAU/USD price and latest saved price from database

--- run_all.py
+++ run_all.py
@@ -1,8 +1,10 @@
 from app.db import Base, engine
 from app.scraper import run_scraper
-from app.gold_price import save_xau_usd
+from app.gold_price import save_xau_usd, get_xau_usd_latest_from_db, get_xau_usd_now
 from app.bubble import calculate_and_save_bubbles
 from app.coin_price import save_coin_price
+from app.transactions import run_transactions_scraper
+
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
@@ -18,3 +20,17 @@ if __name__ == "__main__":
 
     print("\n=== 4. Calculating bubbles ===")
     calculate_and_save_bubbles()
+
+
+    
+    print("\n=== 5. Live price right now ===")
+    print(get_xau_usd_now()) 
+
+    # Latest saved in DB
+    print("\n=== 6. Latest saved in DB ===")
+    row = get_xau_usd_latest_from_db()
+    print(f"{row['date'].strftime('%Y-%m-%d')} | {row['xau_usd']}")
+
+    # add at the end:
+    print("\n=== 5. Fetching fund transactions ===")
+    run_transactions_scraper(days=1)
